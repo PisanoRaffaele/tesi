f26e67053aa7989855d57feec08afb62900843a3
Raffaele Pisano <116729126+PisanoRaffaele@users.noreply.github.com>
2025-06-29 03:12:39 +0200

    Add files via upload

--- finetuneqwen.py
+++ finetuneqwen.py
@@ -8,7 +8,7 @@ import os
 print("🚀 Inizio script...")
 
 data_dir = os.environ['FAST'] + "/rpisano1/dataset/"
-checkpoint_dir = os.environ['FAST'] + "/rpisano1/checkpoints_2_2_chat_mask_on/"
+checkpoint_dir = os.environ['FAST'] + "/rpisano1/checkpoints_2_2_chat_mask_off/"
 
 local_model_path = os.environ['WORK'] + "/rpisano1/models/"
 
@@ -140,13 +140,15 @@ def preprocess_function(example):
 	labels[start_idx:end_idx] = tokenize_label
 
 	attention_mask = [1] * length
-	attention_mask[start_idx:] = [0] * (length - start_idx)
+	# from padding to the end mask is 0
+	num_padding = length - end_idx
+	if num_padding > 0:
+		attention_mask[end_idx:] = [0] * num_padding
 	tokenized_inputs = {'input_ids': input_ids, 'labels': labels, 'attention_mask': attention_mask}
 
 	return tokenized_inputs
 
 print("📚 Caricamento dataset...")
-
 
 dataset = load_dataset("json", data_files=os.path.join(data_dir, json_file), split="train")
 dataset = dataset.filter(
@@ -161,7 +163,7 @@ tokenized_dataset = tokenized_dataset.map(preprocess_function, remove_columns=da
 
 print(tokenized_dataset["train"].column_names)
 
-tokenized_dataset.save_to_disk(f"{data_dir}/tokenized_dataset_2_chat_mask_on")
+tokenized_dataset.save_to_disk(f"{data_dir}/tokenized_dataset_2_chat_mask_off")
 
 # tokenized_dataset = load_from_disk(f"{data_dir}/tokenized_dataset_2_2")
 
